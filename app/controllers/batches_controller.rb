bd7c2976da72f840e108da22c05cd1486baacc20
Fernando Muramoto <fkmuramoto@gmail.com>
2017-08-06 14:54:04 -0300

    controller done

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,16 +1,34 @@
 class BatchesController < ApplicationController
+  before_action :set_batch, only: [ :show, :destroy ]
+
   def index
+    @batches = Batch.all
   end
 
   def show
   end
 
   def new
+    @batch = Batch.new
   end
 
   def create
+    @batch = Batch.new(batch_params)
+    @batch.save
+    redirect_to batches_path
   end
 
   def destroy
+    @batch.destroy
+  end
+
+  private
+
+  def set_batch
+    @batch = Batch.find(params[:id])
+  end
+
+  def batch_params
+    params.require(:batch).permit(:name)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
+  before_action :set_batch, only: [ :new, :create ]
+  before_action :set_post, only: [ :show, :destroy ]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.batch = @batch
+    @post.save
+    redirect_to batches_path
   end
 
   def destroy
+    @post.destroy
+  end
+
+  private
+
+  def set_batch
+    @batch = Batch.find(params[:batch_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:name)
   end
 end
